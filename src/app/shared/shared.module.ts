e6d429cfb1b838984ce8bfcb683188029f17176b
pawel.wysokinski <pawel.wysokinski@tdt.gov.pl>
2020-10-08 15:53:57 +0200

    misja 11 custom pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SpaceImageDirective} from './space-image.directive';
+import {TickizePipe} from './tickize.pipe';
 
 
 @NgModule({
-  declarations: [SpaceImageDirective],
+  declarations: [SpaceImageDirective, TickizePipe],
   imports: [
     CommonModule
   ],
-  exports: [SpaceImageDirective]
+  exports: [SpaceImageDirective, TickizePipe]
 })
 export class SharedModule {
 }
