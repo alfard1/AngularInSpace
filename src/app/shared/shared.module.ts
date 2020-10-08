3c97becc4ce7f347d57dc06c4b0f5a5b867b58de
pawel.wysokinski <pawel.wysokinski@tdt.gov.pl>
2020-10-08 15:50:04 +0200

    misja 11 powiększanie IMG w trakcie poruszania kursorem

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SpaceImageDirective} from './space-image.directive';
+
+
+@NgModule({
+  declarations: [SpaceImageDirective],
+  imports: [
+    CommonModule
+  ],
+  exports: [SpaceImageDirective]
+})
+export class SharedModule {
+}
--- src/app/space/space.module.ts
+++ src/app/space/space.module.ts
@@ -2,12 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {SpaceRoutingModule} from './space-routing.module';
-import {HangarComponent} from './hangar/hangar.component';
 import {SpaceShipComponent} from './space-ship/space-ship.component';
 import {PilotComponent} from './pilot/pilot.component';
 import {PilotRoomComponent} from './pilot-room/pilot-room.component';
 import {ReactiveFormsModule} from '@angular/forms';
-import { EngineersRoomComponent } from './engineers-room/engineers-room.component';
+import {EngineersRoomComponent} from './engineers-room/engineers-room.component';
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   declarations: [SpaceShipComponent, PilotComponent, PilotRoomComponent, EngineersRoomComponent],
@@ -19,7 +19,8 @@ import { EngineersRoomComponent } from './engineers-room/engineers-room.componen
   imports: [
     CommonModule,
     SpaceRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class SpaceModule {
